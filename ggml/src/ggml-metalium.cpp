978d2d1178a9648806c55e57e5c9de59c9893b76
marty1885 <marty188586@gmail.com>
2024-07-17 11:10:48 -0400

    slightly optimize tensot2ggml

--- ggml/src/ggml-metalium.cpp
+++ ggml/src/ggml-metalium.cpp
@@ -325,16 +325,19 @@ void tensor2ggml(const tt::tt_metal::Tensor& tensor, void* dst, [[maybe_unused]]
     for(size_t w = 0; w < shape[0]; w++) {
         for(size_t z = 0; z < shape[1]; z++) {
             for(size_t y = 0; y < shape[2]; y++) {
-                for(size_t x = 0; x < shape[3]; x++) {
-                    const size_t src_idx = w * stride[0] + z * stride[1] + y * stride[2] + x * stride[3];
-                    if(src_dst_same) {
-                        mempcpy((SrcType*)intermid + idx, buf.data() + src_idx, sizeof(SrcType));
-                    }
-                    else {
+                if(src_dst_same) {
+                    // optimization: copy a chunk of memory at a time
+                    const size_t src_idx = w * stride[0] + z * stride[1] + y * stride[2];
+                    memcpy((SrcType*)intermid + idx, buf.data() + src_idx, sizeof(SrcType) * shape[3]);
+                    idx += shape[3];
+                }
+                else {
+                    for(size_t x = 0; x < shape[3]; x++) {
+                        const size_t src_idx = w * stride[0] + z * stride[1] + y * stride[2] + x * stride[3];
                         float val = src_adaptor(buf[src_idx]);
                         ((float*)intermid)[idx] = val;
+                        idx++;
                     }
-                    idx++;
                 }
             }
         }
